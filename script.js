e66b6268eb1f3880bb76d2dfeb3d7dd1cc610817
optimizedmedia44-crypto <optimizedmedia44@gmail.com>
2025-10-14 18:01:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-// Placeholder for future interactivity like chatbots
-console.log("Vidspire is ready!");
+// Just a placeholder for future chatbot, uploads, and folder logic.
+console.log("Vidspire loaded successfully 🌈✨");
