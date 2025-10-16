6ecedc6f52e34ccfa655d809263f09d97364dc89
optimizedmedia44-crypto <optimizedmedia44@gmail.com>
2025-10-16 15:12:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+// Rainbow buttons click feedback
 document.querySelectorAll(".rainbow-btn").forEach(button => {
   button.addEventListener("click", () => {
     alert(`You clicked "${button.textContent.trim()}"!`);
   });
 });
-
