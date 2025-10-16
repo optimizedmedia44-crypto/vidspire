89ef2a31599e59561751a9d4d6b097aa1d5284ae
optimizedmedia44-crypto <optimizedmedia44@gmail.com>
2025-10-16 15:29:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll(".rainbow-btn").forEach(button => {
+document.querySelectorAll(".gradient-btn").forEach(button => {
   button.addEventListener("click", () => {
     alert(`You clicked "${button.textContent.trim()}"!`);
   });
