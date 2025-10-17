3a97942fb5380debfb1cbcbed1ed891e4f71904b
optimizedmedia44-crypto <optimizedmedia44@gmail.com>
2025-10-17 14:28:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// Buttons alert placeholder
-document.querySelectorAll(".category-btn").forEach(button => {
+document.querySelectorAll(".gradient-btn").forEach(button => {
   button.addEventListener("click", () => {
     alert(`You clicked "${button.textContent.trim()}"!`);
   });
 });
-
-console.log("Vidspire is loading... ready to inspire!");
